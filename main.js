2246d36a39d3844f21167129ea6a817de13e90e0
soransh <57911107+soransh-singh@users.noreply.github.com>
2022-11-04 14:44:13 +0530

    update

--- main.js
+++ main.js
@@ -1,44 +1,45 @@
-function Upload() {
-    var fileUpload = document.querySelector("#fileUpload");
-
-    var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
-
-    if (regex.test(fileUpload.value.toLowerCase())) {
-        if (typeof (FileReader) != "undefined") {
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                var table = document.createElement("table");
-                var rows = e.target.result.split("\n");
-                for (var i = 0; i < rows.length; i++) {
-                    var cells = rows[i].split(",");
-                    if (cells.length > 1) {
-                        var row = table.insertRow(-1);
-                        for (var j = 0; j < cells.length; j++) {
-                            var cell = row.insertCell(-1);
-                            cell.innerHTML = cells[j];
-                        }
-                    }
-                }
-                var csv = document.getElementById("csv");
-                csv.innerHTML = "";
-                csv.appendChild(table);
-            }
-            reader.readAsText(fileUpload.files[0]);
-        } else {
-            
-            error.innerHTML = "This browser does not support HTML5."
-        }
-    } else {
-        error.innerHTML = "Please upload a valid CSV file."
-    }
-}
-
 window.onload = (e)=>{
     const btn = document.querySelector("form");
     const error = document.querySelector(".error");
-    error.innerHTML = ""
+    
+    function Upload() {
+        var fileUpload = document.querySelector("#fileUpload");
+
+        var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
+
+        if (regex.test(fileUpload.value.toLowerCase())) {
+            if (typeof (FileReader) != "undefined") {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    var table = document.createElement("table");
+                    var rows = e.target.result.split("\n");
+                    for (var i = 0; i < rows.length; i++) {
+                        var cells = rows[i].split(",");
+                        if (cells.length > 1) {
+                            var row = table.insertRow(-1);
+                            for (var j = 0; j < cells.length; j++) {
+                                var cell = row.insertCell(-1);
+                                cells[j]=cells[j].replaceAll('"','')
+                                cell.innerHTML = cells[j];
+                            }
+                        }
+                    }
+                    var csv = document.getElementById("csv");
+                    csv.innerHTML = "";
+                    csv.appendChild(table);
+                }
+                reader.readAsText(fileUpload.files[0]);
+            } else {
+                
+                error.innerHTML = "This browser does not support HTML5."
+            }
+        } else {
+            error.innerHTML = "Please upload a valid CSV file."
+        }
+    }
     btn.addEventListener("submit", (e)=>{
         e.preventDefault();
+        error.innerHTML = ""
         console.log("btn clicked");
         Upload()
     })
